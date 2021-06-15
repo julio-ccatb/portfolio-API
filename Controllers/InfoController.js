41069a202daa5bb9ca45eff96043f395b85daafa
benazir suriel <benazirsuriel@benazirs-MacBook-Pro.local>
2021-06-15 17:18:11 -0400

    CV route added

--- Controllers/InfoController.js
+++ Controllers/InfoController.js
@@ -1,7 +1,7 @@
 const Code = require("../libs/responses")
 const { Experience } = require("../models/experience");
 const { Project } = require("../models/project");
-
+const fs = require('fs')
 
 var Controller = {
 
@@ -29,7 +29,28 @@ var Controller = {
             console.log(err);
             res.status(500).send({ message: Code._500 });
         }
-    }
+    },
+
+    GetCurriculum: async (req, res) => {
+
+
+        let curriculum = '../resources/docs/Curriculum_JCCA.pdf'
+        let path = `${__dirname}/${curriculum}`
+
+        fs.access(path, fs.constants.F_OK, err => {
+            if (err) return res.status(404).send({ message: Code._404, err })
+            console.log(`${path} ${err ? 'dont exist' : 'exist'}`);
+        });
+
+
+        fs.readFile(path, (err, file) => {
+            if (err) return res.status(500).send({ message: Code._500, err });
+            res.writeHead(200, { "Content-type": "application/pdf" })
+            return res.end(file)
+        })
+
+
+    },
 
 }
 
--- Routes/Info.js
+++ Routes/Info.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const Info_Router = Router()
-const { GetInfo, GetProjects } = require("../Controllers/InfoController");
+const { GetInfo, GetProjects, GetCurriculum, test } = require("../Controllers/InfoController");
 
 
 //METHODS
 
 //GET
 Info_Router.get('/info', GetInfo);
+Info_Router.get('/cvitae', GetCurriculum);
 Info_Router.get('/projects', GetProjects);
 
 
